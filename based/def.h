b071b9db430ccbbb9524d9cbd3515c418d9881a3
jianpanlanyue <jianpanlanyue@163.com>
2015-09-12 10:51:01 +0800

    modified:   def.h

--- based/def.h
+++ based/def.h
@@ -4,8 +4,15 @@
 #define NAMES_CAT_IMPL(NAME1,NAME2) NAME1##NAME2
 #define NAMES_CAT(NAME1, NAME2) NAMES_CAT_IMPL(NAME1,NAME2)
 
-#if defined(_UNICODE) || defined(UNICODE)
-	#define tstring std::wstring
-#else
-	#define tstring std::string
+#ifndef tstring
+	#if defined(_UNICODE) || defined(UNICODE)
+		#define tstring std::wstring
+	#else
+		#define tstring std::string
+	#endif
+#endif
+
+#ifndef NAMESPACE_BEGIN
+#define NAMESPACE_BEGIN(SPACE_NAME) namespace SPACE_NAME{
+#define NAMESPACE_END }
 #endif
